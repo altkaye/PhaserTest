cc0407d30e28ffeaeb2fcc9bcff02c6cd46440d8
relva.org <info@relva.org>
2015-11-13 09:28:22 +0900

    init phaser

--- src/test.js
+++ src/test.js
@@ -1,3 +1,15 @@
-(function() {
+(function () {
     alert("hello");
+    console.log("hello");
+    var game = new Phaser.Game(640, 480, Phaser.AUTO, '', {
+        preload: function () {
+            console.log("on preload");
+        },
+        create: function () {
+            console.log("on create");
+        },
+        update: function () {
+
+        }
+    });
 })();
